3a8f547f5ba1fbd40079d8723e36acce0bbb63bc
DESKTOP-VVKT5NE\dinht <dinhthien18052003@gmail.com>
2023-07-20 08:44:52 +0700

    register

---
+++ App_client/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using App_Shared.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace App_client.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public RegisterController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        
+        public async Task<IActionResult> Register(RegisterUser registerUser, string role)
+        {
+            // Convert registerUser to JSON
+            var registerUserJSON = JsonConvert.SerializeObject(registerUser);
+
+            // Convert to string content
+            var stringContent = new StringContent(registerUserJSON, Encoding.UTF8, "application/json");
+
+            // Add role to queryString
+            role = "User";
+            var queryString = $"?role={role}";
+            ////https://localhost:7149/api/Register?role=User
+            // Send request POST to register API
+            var response = await _httpClient.PostAsync($"https://localhost:7149/api/Register{queryString}", stringContent);
+
+            ViewBag.Message = await response.Content.ReadAsStringAsync();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
